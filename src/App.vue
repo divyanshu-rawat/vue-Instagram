8f5bd9f74e8fdde2f6cc860f309b65c54d2b96f4
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-15 18:18:07 +0530

    updated css, added bootstrap-vue, font awesome, saas loader and node sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-phone">
+      <div class="phone-header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+      </div>
+      <phone-body />
+      <div class="phone-footer">
+       <div class="home-cta">
+        <i class="fa fa-home fa-lg"></i>
+       </div>
+       <div class="upload-cta">
+        <i class="fa fa-plus-square fa-lg"></i>
+
+       </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PhoneBody from "./components/phone-body"
+import 'font-awesome/css/font-awesome.css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    "phone-body": PhoneBody
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" src="./styles/app.scss"></style>
+
+<style type="text/css">
+  body{
+    font-family: 'Open Sans', sans-serif;
 }
 </style>
---
+++ src/components/phone-body.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <div class="phone-body">
+    This is the Phone Body
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhoneBody"
+};
+</script>
+
+<style lang="scss" src="../styles/phone-body.scss">
+
+</style>
